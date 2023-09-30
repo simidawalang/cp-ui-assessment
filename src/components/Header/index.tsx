79c27a5a3040a96bc493294ab2b42a7f5540e80e
L I L V C <simidawalang@gmail.com>
2023-09-30 22:58:23 +0100

    created select component in header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,8 +1,36 @@
+import { useState } from "react";
+import Select from "../Select";
+
 const Header = () => {
-  return <header className="header">
-    <h3 className="heading-text">London Internship Program</h3>
-    <span className="city">London</span>
-  </header>;
+  const [showOptions, setShowOptions] = useState(false);
+  const selectOptions = [
+    { id: 1, value: "Applied", count: 1745 },
+    { id: 2, value: "Shortlisted", count: 453 },
+    { id: 3, value: "Technical Interview", count: 123 },
+    { id: 4, value: "Opportunity Browsing", count: 243 },
+    { id: 5, value: "Video Interview I", count: 25 },
+    { id: 6, value: "Video Interview II", count: 25 },
+    { id: 7, value: "Video Interview III", count: 25 },
+    { id: 8, value: "Offer", count: 25 },
+    { id: 9, value: "Withdrawn", count: 25 },
+  ];
+  const [selectValue, setSelectValue] = useState("Opportunity Browsing");
+
+  return (
+    <header className="header">
+      <div>
+        <h3 className="heading-text">London Internship Program</h3>
+        <span className="city">London</span>
+      </div>
+      <Select
+        setShowOptions={setShowOptions}
+        showOptions={showOptions}
+        options={selectOptions}
+        value={selectValue}
+        setValue={setSelectValue}
+      />
+    </header>
+  );
 };
 
 export default Header;
--- src/components/Select/index.tsx
+++ src/components/Select/index.tsx
@@ -1,21 +1,47 @@
+import { Dispatch, MouseEventHandler, SetStateAction } from "react";
+import { FaAngleDown } from "react-icons/fa";
+
 const Select = ({
   value,
+  setValue,
   options,
-  showOption,
+  showOptions,
+  className,
+  setShowOptions,
 }: {
   value: string;
+  setValue: Dispatch<SetStateAction<string>>;
   options: any[];
-  showOption: boolean;
+  showOptions: boolean;
+  className?: string;
+  setShowOptions: Dispatch<SetStateAction<boolean>>;
 }) => {
   return (
-    <div className="select">
-      <span className="select-value">{value}</span>
-      {showOption && (
+    <div
+      className="select"
+      onClick={(e) => {
+        e.stopPropagation();
+        setShowOptions(true);
+      }}
+    >
+      <div className="select-value">
+        {value} <FaAngleDown color="#1D4ED8" />
+      </div>
+      {showOptions && (
         <div className="select-option__container">
           {options.map((o) => (
-            <div key={o.id} className="select-option">
+            <div
+              key={o.id}
+              className={`select-option ${value === o.value ? "active" : ""}`}
+              onClick={(e) => {
+                e.stopPropagation();
+                setValue(o.value);
+                setShowOptions(false);
+                console.log(1);
+              }}
+            >
               {o.value}
-              <span>{o.count}</span>
+              <span className="count">{o.count}</span>
             </div>
           ))}
         </div>
